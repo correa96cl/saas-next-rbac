3e6178c133948e5ee8380c451ed96f92a7e9d24b
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-08-13 20:23:40 -0300

    Fix: Sign In

---
+++ next-saas-rbac/apps/web/src/app/auth/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { FormSignIn } from "./sign-in-form";
+
+export default function SignInPage() {
+
+
+
+    return <FormSignIn/>
+}
---
+++ next-saas-rbac/apps/web/src/app/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    < Button>Pressione se acha a Renata mal-carater e BURRRAA</Button>
+  );
+}
