0880fd8162384dc65f2e5ffe0840fe350443ad5f
Marcelo Valderrama <marcelovalderramacorrea@MacBook-Air-de-Marcelo.local>
2024-08-17 19:02:29 -0300

    Commit Header organizations

--- next-saas-rbac/apps/web/src/components/header.tsx
+++ next-saas-rbac/apps/web/src/components/header.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import rocketseatIcon from '@/app/assets/rocketseat-icon.svg';
 import { ProfileButton } from "./profile-button";
+import { Slash } from "lucide-react";
+import { OrganizationSwitcher } from "./organization-switcher";
 export default function Header(){
     return (
         <div className="mx-auto flex max-w-[1200px] items-center justify-between">
             <div className="flex items-center gap-1">
             <Image alt="Rocketseat" src={rocketseatIcon} className="size-6 dark:invert"/>
+            <Slash className="size-3 -rotate-[24deg] text-border"/>
+            <OrganizationSwitcher/>
             </div>
 
             <div className="flex items-center gap-4">
